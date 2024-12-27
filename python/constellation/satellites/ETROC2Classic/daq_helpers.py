234097b8cc3f5c7ab2e0bbc19814461dfd2d4f2c
Murtaza Safdari <murtaza.safdari.1@gmail.com>
2024-12-27 12:04:12 -0600

    helper files

--- python/constellation/satellites/ETROC2Classic/daq_helpers.py
+++ python/constellation/satellites/ETROC2Classic/daq_helpers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import command_interpret
+
 #========================================================================================#
 '''
 @author: Murtaza Safdari
