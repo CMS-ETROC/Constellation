6ec4fb3b09106db0e48ff3af156f6e28fce62a42
Murtaza Safdari <murtaza.safdari.1@gmail.com>
2024-12-29 13:56:08 -0600

    ETROC2Receiver for inital nem testing

--- python/constellation/satellites/ETROC2Classic/__main__.py
+++ python/constellation/satellites/ETROC2Classic/__main__.py
@@ -6,7 +6,6 @@ Provides the entry point for the Mariner example satellite
 """
 
 from constellation.core.base import setup_cli_logging, EPILOG
-# from constellation.core.satellite import SatelliteArgumentParser
 from constellation.core.datasender import DataSenderArgumentParser
 
 from .ETROC2Classic import ETROC2Classic
